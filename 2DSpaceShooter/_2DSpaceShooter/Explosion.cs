d93ad3376973c24a03a870bb757b4d8b80be47a4
ncminhqn <39033394+ncminhqn@users.noreply.github.com>
2018-07-07 05:07:07 +0700

    finish

--- 2DSpaceShooter/_2DSpaceShooter/Explosion.cs
+++ 2DSpaceShooter/_2DSpaceShooter/Explosion.cs
@@ -2,10 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace _2DSpaceShooter
 {
     class Explosion
     {
+        public Texture2D texture;
+        public Vector2 position;
+        public float timer;
+        public float interval;
+        public Vector2 origin;
+        public int currentFrame, spriteWidth, spriteHeight;
+        public Rectangle sourceRectangle;
+        public bool isVisible;
+
+        // Constructor
+        public Explosion(Texture2D newTexture, Vector2 newPosition)
+        {
+            position = newPosition;
+            texture = newTexture;
+            timer = 0f;
+            interval = 15f;
+            currentFrame = 1;
+            spriteWidth = 64;
+            spriteHeight = 64;
+            isVisible = true;
+        }
+
+        // Load Content
+        public void LoadContent(ContentManager Content)
+        {
+        }
+
+        // Update
+        public void Update(GameTime gameTime)
+        {
+            // Increase the timer by the number of milliseconds since update was last called
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // Check the timer is more than the chosen interval
+            if (timer > interval)
+            {
+                // Show next frame
+                currentFrame++;
+                // Reser Timer
+                timer = 0f;
+            }
+
+            // If were on the last frame, make the explosion invisible and reset currentFrame to beginning of spritesheet
+            if (currentFrame == 10)
+            {
+                isVisible = false;
+                currentFrame = 0;
+            }
+
+            sourceRectangle = new Rectangle(currentFrame * spriteWidth, 0, spriteWidth, spriteHeight);
+            origin = new Vector2(sourceRectangle.Width / 2, sourceRectangle.Height / 2);
+        }
+
+        // Draw
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // if visible then draw
+            if(isVisible == true)
+            {
+                spriteBatch.Draw(texture, position, sourceRectangle, Color.White, 0f, origin, 1.0f, SpriteEffects.None, 0);
+            }
+        }
     }
 }
--- 2DSpaceShooter/_2DSpaceShooter/Game1.cs
+++ 2DSpaceShooter/_2DSpaceShooter/Game1.cs
@@ -14,20 +14,32 @@ namespace _2DSpaceShooter
     // Main 
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        // State Enum
+        public enum State
+        {
+            Menu,
+            Playing, 
+            Gameover
+        }
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Random random = new Random();
         public int enemyBulletDamage;
+        public Texture2D menu;
+        public Texture2D gameover;
 
         // Lists
         List<Asteroid> asteroidList = new List<Asteroid>();
         List<Enemy> enemyList = new List<Enemy>();
+        List<Explosion> explosionList = new List<Explosion>();
 
         // Instantiating our Player and Starfield objects
         Player player = new Player();
         Starfield starfield = new Starfield();
         HUD hud = new HUD();
 
+        // Set first State
+        State gameState = State.Menu;
         
         // Constructor
         public Game1()
@@ -39,13 +51,13 @@ namespace _2DSpaceShooter
             this.Window.Title = "2D Space Shooter";
             Content.RootDirectory = "Content";
             enemyBulletDamage = 1;
+            menu = null;
+            gameover = null;
         }
         
         // Init
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
         
@@ -54,21 +66,18 @@ namespace _2DSpaceShooter
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
             hud.LoadContent(Content);
             player.LoadContent(Content);
             starfield.LoadContent(Content);
-
-            
+            menu = Content.Load<Texture2D>("menu");
+            gameover = Content.Load<Texture2D>("gameover");
         }
         
         // Unload Content
         protected override void UnloadContent()
         {
-            
         }
         
-
         // Update
         protected override void Update(GameTime gameTime)
         {
@@ -76,101 +85,189 @@ namespace _2DSpaceShooter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            // Updating Enemy's and checking collision of enemyShip to playerShip
-            foreach(Enemy e in enemyList)
+            // Updating playing state
+            switch (gameState)
             {
-                // Check if enemyShip is colliding with player
-                if (e.boundingBox.Intersects(player.boundingBox))
-                {
-                    player.life -= 1;
-                    e.isVisible = false;
-                }
-
-                // Check enemy bullet collision with player ship
-                for(int i = 0; i < e.bulletList.Count; i++)
-                {
-                    if(player.boundingBox.Intersects(e.bulletList[i].boundingBox))
+                case State.Playing:
                     {
-                        player.life -= enemyBulletDamage;
-                        e.bulletList[i].isVisible = false;
-                    }
-                }
+                        // Updating Enemy's and checking collision of enemyShip to playerShip
+                        starfield.speed = 5;
+                        foreach (Enemy e in enemyList)
+                        {
+                            // Check if enemyShip is colliding with player
+                            if (e.boundingBox.Intersects(player.boundingBox))
+                            {
+                                player.life -= 1;
+                                e.isVisible = false;
+                            }
 
-                // Check player bullet collision to enemy ship
-                for (int i = 0; i < player.BulletList.Count; i++)
-                {
-                    if(player.BulletList[i].boundingBox.Intersects(e.boundingBox))
+                            // Check enemy bullet collision with player ship
+                            for (int i = 0; i < e.bulletList.Count; i++)
+                            {
+                                if (player.boundingBox.Intersects(e.bulletList[i].boundingBox))
+                                {
+                                    player.life -= enemyBulletDamage;
+                                    e.bulletList[i].isVisible = false;
+                                }
+                            }
+
+                            // Check player bullet collision to enemy ship
+                            for (int i = 0; i < player.BulletList.Count; i++)
+                            {
+                                if (player.BulletList[i].boundingBox.Intersects(e.boundingBox))
+                                {
+                                    explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion"), new Vector2(e.position.X, e.position.Y)));
+                                    hud.playerScore += 10;
+                                    player.BulletList[i].isVisible = false;
+                                    e.isVisible = false;
+                                }
+                            }
+
+                            e.Update(gameTime);
+                        }
+
+                        // Update Explosions
+                        foreach (Explosion ex in explosionList)
+                        {
+                            ex.Update(gameTime);
+                        }
+
+                        // foreach asteroid in our asteroidList, update and check for collision
+                        foreach (Asteroid a in asteroidList)
+                        {
+                            // Check to see if any of the asteroids are colliding with our playership, if they are.. set isVisible to False(remove them from the asteroidList)
+                            if (a.boundingBox.Intersects(player.boundingBox))
+                            {
+                                player.life -= 1;
+                                a.isVisible = false;
+                            }
+
+                            // Interate through our bulletList if any asteroids come in contacts with these bullets, destroy bullet and asteroid
+                            for (int i = 0; i < player.BulletList.Count; i++)
+                            {
+                                if (a.boundingBox.Intersects(player.BulletList[i].boundingBox))
+                                {
+                                    explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion"), new Vector2(a.position.X, a.position.Y)));
+                                    hud.playerScore += 5;
+                                    a.isVisible = false;
+                                    player.BulletList.ElementAt(i).isVisible = false;
+                                }
+                            }
+
+                            a.Update(gameTime);
+                        }
+
+
+                        //hud.Update(gameTime);
+
+                        // If playerlife hits 0 then go to gameover state
+                        if (player.life <= 0)
+                            gameState = State.Gameover;
+
+                        player.Update(gameTime);
+                        starfield.Update(gameTime);
+                        ManageExplosions();
+                        LoadAsteroids();
+                        LoadEnemies();
+                        break;
+                    }
+                    // Updating menu state
+                case State.Menu:
                     {
-                        hud.playerScore += 10;
-                        player.BulletList[i].isVisible = false;
-                        e.isVisible = false;
-                    }
-                }
+                        // Get Keyboard State
+                        KeyboardState keyState = Keyboard.GetState();
 
-                e.Update(gameTime);
+                        if(keyState.IsKeyDown(Keys.Space))
+                        {
+                            gameState = State.Playing;
+                        }
+                        starfield.Update(gameTime);
+                        starfield.speed = 1;
+                        break;
+                    }
+                    // Updating gameover state
+                case State.Gameover:
+                    {
+                        // Get Keyboard State
+                        KeyboardState keyState = Keyboard.GetState();
+
+                        // If in the gameover screen and user hits "Escape" key, Return to the main menu
+                        if (keyState.IsKeyDown(Keys.Escape))
+                        {
+                            player.position = new Vector2(400, 800);
+                            enemyList.Clear();
+                            asteroidList.Clear();
+                            player.life = 10;
+                            hud.playerScore = 0;
+                            gameState = State.Menu;
+                        }
+                        starfield.Update(gameTime);
+                        starfield.speed = 1;
+                        break;
+                    }
             }
             
-            // foreach asteroid in our asteroidList, update and check for collision
-            foreach(Asteroid a in asteroidList)
-            {
-                // Check to see if any of the asteroids are colliding with our playership, if they are.. set isVisible to False(remove them from the asteroidList)
-                if(a.boundingBox.Intersects(player.boundingBox))
-                {
-                    player.life -= 1;
-                    a.isVisible = false;
-                }
-
-                // Interate through our bulletList if any asteroids come in contacts with these bullets, destroy bullet and asteroid
-                for (int i = 0; i < player.BulletList.Count; i++ )
-                {
-                    if(a.boundingBox.Intersects(player.BulletList[i].boundingBox))
-                    {
-                        hud.playerScore += 5;
-                        a.isVisible = false;
-                        player.BulletList.ElementAt(i).isVisible = false;
-                    }
-                }
-
-                a.Update(gameTime);
-            }
-
-
-            //hud.Update(gameTime);
-            player.Update(gameTime);
-            starfield.Update(gameTime);
-            LoadAsteroids();
-            LoadEnemies();
-
             base.Update(gameTime);
         }
         
         // Draw
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
             
-            // background
-            starfield.Draw(spriteBatch);
+            switch(gameState)
+            {
+                // Drawing playing state
+                case State.Playing:
+                    {
+                        // background
+                        starfield.Draw(spriteBatch);
+
+                        // Asteroids
+                        foreach (Asteroid a in asteroidList)
+                        {
+                            a.Draw(spriteBatch);
+                        }
+
+                        foreach (Explosion ex in explosionList)
+                        {
+                            ex.Draw(spriteBatch);
+                        }
+
+                        // PlayerShip, Life
+                        player.Draw(spriteBatch);
+
+                        // EnemiesShip
+                        foreach (Enemy e in enemyList)
+                        {
+                            e.Draw(spriteBatch);
+                        }
+
+                        // HUD: score
+                        hud.Draw(spriteBatch);
+
+                        break;
+                    }
+
+                    // Drawing menu state
+                case State.Menu:
+                    {
+                        starfield.Draw(spriteBatch);
+                        spriteBatch.Draw(menu, new Vector2(0, 0), Color.White);
+                        break;
+                    }
+
+                    // Drawing gameover state
+                case State.Gameover:
+                    {
+                        starfield.Draw(spriteBatch);
+                        spriteBatch.Draw(gameover, new Vector2(0, 0), Color.White);
+                        spriteBatch.DrawString(hud.playerScoreFont, "Your Final Score: " + hud.playerScore.ToString(), new Vector2(240, 400), Color.Red);
+                        break;
+                    }
+            }
             
-            // Asteroids
-            foreach (Asteroid a in asteroidList)
-            {
-                a.Draw(spriteBatch);
-            }
-
-            // PlayerShip, Life
-            player.Draw(spriteBatch);
-
-            // EnemiesShip
-            foreach(Enemy e in enemyList)
-            {
-                e.Draw(spriteBatch);
-            }
-
-            // HUD: score
-            hud.Draw(spriteBatch);
-
             spriteBatch.End();
 
             base.Draw(gameTime);
@@ -219,6 +316,19 @@ namespace _2DSpaceShooter
                 if (!enemyList[i].isVisible)
                 {
                     enemyList.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        // Manage Explosions
+        public void ManageExplosions()
+        {
+            for(int i = 0; i < explosionList.Count; i++)
+            {
+                if(!explosionList[i].isVisible)
+                {
+                    explosionList.RemoveAt(i);
                     i--;
                 }
             }
